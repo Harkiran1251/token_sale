a6e36d2cd4a6bcb8f6aa3ba612d1d8704e297876
Harkiran <harkiran@gmail.com>
2019-05-14 14:26:08 +0530

    DelegatedTransfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -14,7 +14,21 @@ address indexed _to,
 uint256 _value
 );
 
+
+event Approval(
+	address indexed _owner,
+	address indexed _spender,
+	uint256 _value
+
+);
+//approve event also
+
 mapping(address=>uint256) public balanceOf;
+
+//allowance
+
+mapping(address=>mapping(address=>uint256)) public allowance;
+
 constructor(uint256 _initialSupply) public{
     
     balanceOf[msg.sender]=_initialSupply;//
@@ -38,8 +52,48 @@ constructor(uint256 _initialSupply) public{
 
 	 emit Transfer(msg.sender,_to,_value);
 	 //returnboolean value
-	 return true;
+	 return true; 
 
 	}
 
-} 
+	//approve fxn
+	function approve(address _spender,uint256 _value)public returns (bool success){
+
+	//allowance
+	allowance[msg.sender][_spender]=_value;
+
+	//approve event
+
+	emit Approval(msg.sender,_spender,_value);
+	return true;
+	
+	
+	}
+
+	//transferfrom fxn
+
+	function transferFrom(address _from,address _to,uint256 _value)public returns (bool success){
+	
+	//require _from has enough tokens
+
+	require(_value <= balanceOf[_from]);
+//require allowance is big enough
+
+	require(_value <= allowance[_from][msg.sender]);
+//change the balance
+
+	balanceOf[_from]-=_value;
+	balanceOf[_to]+=_value;
+//update the allowance
+
+	allowance[_from][msg.sender]-=_value;
+//transfer event
+	emit Transfer(_from,_to,_value);
+//returna  Boolean value
+
+return true;
+
+	}
+
+
+}
