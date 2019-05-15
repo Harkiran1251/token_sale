7191b5d19b774af91ab1c36a167ca22804427024
Harkiran <harkiran@gmail.com>
2019-05-15 16:55:12 +0530

    4.Token Sale

---
+++ contracts/DappTokenSale.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.0;
+
+import "./DappToken.sol";
+contract DappTokenSale{
+
+	address payable admin;
+    DappToken public tokenContract;
+	uint256 public tokenPrice;
+	uint256 public tokensSold;
+
+
+	event Sell(
+	address _buyer,
+	uint256 _amount
+	);
+
+    constructor(DappToken _tokenContract,uint256 _tokenPrice)public{
+        
+        //assign admin
+
+		admin=msg.sender;
+        //token contract
+		tokenContract=_tokenContract;
+
+		tokenPrice=_tokenPrice;
+        //token price
+    }
+
+
+	//multiply fxn
+
+	function multiply(uint x,uint y)internal pure returns (uint z){
+	require(y==0||(z=x*y)/y==x);
+	}
+
+	//Buy Tokens
+
+	function buyTokens(uint256 _numberOfTokens)public payable{
+	
+	//require that value is equal to tokens
+	require(msg.value == multiply(_numberOfTokens,tokenPrice));
+
+	//require taht the contracthas enough tokens
+	require(tokenContract.balanceOf(address(this))>=_numberOfTokens);
+
+	//require that a transfer is successful
+
+	require(tokenContract.transfer(msg.sender,_numberOfTokens));
+	//keep track of tokenssold
+	tokensSold+=_numberOfTokens;
+
+	//trigger sell event
+	emit Sell(msg.sender,_numberOfTokens);
+	}
+
+
+
+	//Ending Token DappSaleToken
+
+	function endSale() public payable{
+	
+	//require admin
+	require(msg.sender==admin);
+
+	//transfer remainingdapp tokens to admin
+
+	require(tokenContract.transfer(admin,tokenContract.balanceOf(address(this))));
+	//destroy the contract
+
+	selfdestruct(msg.sender);
+
+	}
+    
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 const DappToken = artifacts.require("DappToken");
+const DappTokenSale = artifacts.require("DappTokenSale");
 
 module.exports = function (deployer) {
-    deployer.deploy(DappToken,1000000);
+    deployer.deploy(DappToken, 1000000).then(function () {
+
+        //token pric ein ether 0.01ether
+        var tokenPrice = 1000000000000000;;//in wei
+        return deployer.deploy(DappTokenSale,DappToken.address,tokenPrice);
+    });
+        
 };
